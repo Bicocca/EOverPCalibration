5cfbdd52fc37941727ff188b5219ef8875ae0733
Luca Brianza <brianza@hercules.mib.infn.it>
2015-06-27 14:48:43 +0200

    first commit new branch

--- FastCalibrator/submit_job.py
+++ FastCalibrator/submit_job.py
@@ -11,17 +11,23 @@ currentDir = os.getcwd();
 CMSSWDir = "/gwpool/users/brianza/PHD/CMSSW_6_1_1/src/";
 ReducedTreeDir = "";
 
-name = ["EE"];
+name = ["EB"];
+#split = ["split","noSplit"];
+split = ["noSplit","split"];
+cut = ["005"];
 
-for i in range(len(name)):
-    fn = "Job/Job_"+name[i];
+for j in range(len(split)):
+  for i in range(len(name)):
+    print split[j]
+    print name[i]
+    fn = "Job/Job_"+name[i]+cut[i]+"_"+split[j];
     outScript = open(fn+".sh","w");
-    command = "./bin/FastCalibrator"+name[i]+".exe cfg/FastCalibrator_"+name[i]+"_split.cfg";
+    command = "./bin/FastCalibrator"+name[i]+".exe cfg/FastCalibrator_"+name[i]+"_"+split[j]+".cfg";
     outScript.write('#!/bin/bash');
     outScript.write("\n"+'cd '+CMSSWDir);
     outScript.write("\n"+'eval `scram runtime -sh`');
     outScript.write("\n"+'cd '+currentDir);
-    outScript.write("\n"+"unbuffer "+command+" > "+fn+"_output_Oct22_Run2012D_Cal_Dic2012_noOldCorr.txt");
+    outScript.write("\n"+"unbuffer "+command+" > "+fn+"_Norm005.txt");
     outScript.close();
     os.system("chmod 777 "+currentDir+"/"+fn+".sh");
     os.system("qsub -V -d "+currentDir+" -q longcms "+currentDir+"/"+fn+".sh");
